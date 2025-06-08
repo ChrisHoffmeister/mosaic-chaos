048c4ed3bce6ac3cb10d453e07491ad36074119b
H.Off.tian <30471423+ChrisHoffmeister@users.noreply.github.com>
2025-06-08 17:41:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let imgs = [];
 let filenames = [];
 
 function preload() {
-  // Bilder automatisch erzeugen (1–97), die vorhanden sind:
+  // Lade alle Bilder von img1.jpg bis img124.jpg
   for (let i = 1; i <= 124; i++) {
     filenames.push(`img${i}.jpg`);
   }
@@ -15,31 +15,35 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
+  imageMode(CENTER);
   angleMode(RADIANS);
-  imageMode(CENTER); // Zentrierte Platzierung
 }
 
 function draw() {
   background(10);
+
   for (let i = 0; i < imgs.length; i++) {
     let img = imgs[i];
 
-    // Zufällige Position
+    // Zufällige Position auf der Leinwand
     let x = random(width);
     let y = random(height);
 
-    // Zufällige Größe
-    let scale = random(0.4, 1.0);
-    let w = img.width * scale;
-    let h = img.height * scale;
+    // Erhalte Bildseitenverhältnis
+    let aspectRatio = img.width / img.height;
+
+    // Zufällige Zielhöhe (Skalierung)
+    let targetHeight = random(80, 220);
+    let targetWidth = targetHeight * aspectRatio;
 
     // Zufällige Rotation
-    let angle = random(-PI / 8, PI / 8);
+    let angle = random(-PI / 10, PI / 10);
 
+    // Zeichne das Bild mit Drehung und Position
     push();
     translate(x, y);
     rotate(angle);
-    image(img, 0, 0, w, h);
+    image(img, 0, 0, targetWidth, targetHeight);
     pop();
   }
 }
